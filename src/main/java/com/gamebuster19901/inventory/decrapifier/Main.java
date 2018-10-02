b6f1b87c700ecdf46cfa8fd9376cf7ac614f1b18
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2018-10-01 22:19:35 -0400

    Update 1.0.2.1

--- src/main/java/com/gamebuster19901/inventory/decrapifier/Main.java
+++ src/main/java/com/gamebuster19901/inventory/decrapifier/Main.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class Main{
 	public static final String MODID = "invdecrap";
 	public static final String MODNAME = "Inventory Decrapifier";
-	public static final String VERSION = "1.0.0.0 - 1.12.2";
+	public static final String VERSION = "1.0.2.1 - 1.12.2";
 	public static final Logger LOGGER = LogManager.getLogger(MODNAME);
 	private static Main instance;
 	@SidedProxy(serverSide = "com.gamebuster19901.inventory.decrapifier.proxy.ServerProxy", clientSide = "com.gamebuster19901.inventory.decrapifier.proxy.ClientProxy")
